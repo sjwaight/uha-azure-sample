a470e52fdc0a6da1fafbfd8a16b4d8a37f429227
sjwaight <simon.waight@gmail.com>
2014-11-03 11:35:01 +1100

    Final set of changes for blog post.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ uha-azure-sample
 ================
 
 Sample code for an Ultra Highly Available Azure web application
+
+You can find the blog posts covering the purpose and setup of this sample application here:
+
+http://blog.kloud.com.au/2014/11/03/deploy-an-ultra-high-availablity-mvc-web-app-on-microsoft-azure-part-1/
--- web-app-tier/MvcMusicStore/WebRole.cs
+++ web-app-tier/MvcMusicStore/WebRole.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.ServiceRuntime;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace MvcMusicStore
 {
-    public class WebRole
+    public class WebRole : RoleEntryPoint
     {
+        public override bool OnStart()
+        {
+            return base.OnStart();
+        }
     }
 }
